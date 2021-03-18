3dc967bd44d7c6e0ff7ad7e5d224d7cec62e4af2
maxhannan <maxhannan@substanceseven.com>
2021-03-18 00:02:13 -0500

    loading status, design refinements

--- src/Components/ProductBox.jsx
+++ src/Components/ProductBox.jsx
@@ -1,6 +1,6 @@
-import {  Typography, Grid, Paper, makeStyles, IconButton } from "@material-ui/core";
+import {  Typography, Grid, Paper, makeStyles, IconButton, Button } from "@material-ui/core";
 import {Link as RouterLink } from 'react-router-dom'
-import {MdInfoOutline, MdAddCircleOutline} from 'react-icons/md'
+import {MdInfoOutline, MdAddCircleOutline, MdAddShoppingCart} from 'react-icons/md'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -19,7 +19,8 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column', 
     justifyContent: 'flex-start', 
     height: '100%',
-    textAlign: 'left'
+    textAlign: 'left',
+    position: 'relative'
   }
 }));
 
@@ -28,31 +29,27 @@ const ProductBox = ({product}) => {
   return ( 
     <Grid item  xs = {12} sm = {6} md = {4} lg = {3}>
       <Paper className={classes.paper}>
-      <div style = {{position: 'relative'}}>
+      <div >
         <img 
         className={classes.imgStyle} 
         src = {product.pic} alt=""
         onMouseOver={e => (e.currentTarget.src = product.altPic)}
         onMouseOut={e => (e.currentTarget.src = product.pic)}
         />
-        <div style = {{position: 'absolute', bottom: '0', left: '0'}}> 
-          <IconButton color = 'primary'>
-            <MdAddCircleOutline/>
-          </IconButton>
-        </div>
-        <div style = {{position: 'absolute', bottom: '0', right: '0'}}> 
-          <IconButton color = 'secondary' component = {RouterLink} to = '/react-tantalus/details'>
-            <MdInfoOutline/>
-          </IconButton>
-        </div>
+       
       </div>
         <div className={classes.typeContainer}>
-          <Typography variant = 'h6' color = 'textPrimary' gutterBottom>
+          <Typography variant = 'h6' color = 'textPrimary' style = {{fontWeight: '300'}} gutterBottom>
             {product.text}
           </Typography>
           <Typography variant = 'h6' color = 'textSecondary' gutterBottom>
             ${product.price}
           </Typography>
+          <div style = {{position: 'absolute', bottom: '0', right: '0'}}>
+          <IconButton color = 'secondary' component = {RouterLink} to = '/react-tantalus/details'>
+            <MdInfoOutline/>
+          </IconButton>
+        </div>
         </div>
       </Paper>
     </Grid>
--- src/Components/ProductGrid.jsx
+++ src/Components/ProductGrid.jsx
@@ -1,5 +1,5 @@
-import { Container, Grid } from "@material-ui/core";
-import { useEffect } from "react";
+import { Container, Grid, LinearProgress, CircularProgress } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import {productArray} from '../ProductArray'
 import ProductBox from "./ProductBox";
 
@@ -7,15 +7,24 @@ const ProductGrid = ({section = 'men'}) => {
   const filteredProducts = productArray.filter(product => product.sectionId.includes(section))
  
   const ProductElements = () =>{
+    const [loading, setLoading] = useState(true)
     useEffect(()=>{
-      console.log('Mount')
+      setTimeout(() =>  setLoading(false) , 1500);
+
     },[])
   return(
-    <Grid container spacing={3} style = {{animation: 'appear 500ms ease-in-out'}}>
+    <>
+    {loading &&  
+    <div style = {{display: 'flex', height: '90vh', justifyContent: 'center', alignItems: 'center'}}>
+    <CircularProgress color="secondary" size = '3rem' />
+    </div>
+    }
+    {!loading && <Grid container spacing={3} style = {{animation: 'appear 500ms ease-in-out'}}>
       {filteredProducts.map(product => (
         <ProductBox key = {product.id} product = {product}/>
       ))}
-    </Grid>
+    </Grid>}
+    </>
   );
   }
   
